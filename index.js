2f31568708e6067066340c466cdc87ab40e4d4c7
Galaad <75391703+ImGalaad@users.noreply.github.com>
2022-03-06 19:36:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-require('dotenv').config();
 const Mustache = require('mustache');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
-// DATA contains all the data to be provided to Mustache.
-
 let DATA = {
-    name: 'Galaad',
-    date: new Date().toLocaleDateString('fr-FR', {
+  name: 'Galaad',
+  date: new Date().toLocaleDateString('fr-FR', {
     weekday: 'long',
     month: 'long',
     day: 'numeric',
@@ -19,31 +16,27 @@ let DATA = {
   }),
 };
 
-// Open 'main.mustache'
-// Ask Mustache to render our file with the data
-// Create a README.md file with the generated output
-
 async function setWeatherInformation() {
-    await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
-    )
-      .then(r => r.json())
-      .then(r => {
-        DATA.city_temperature = Math.round(r.main.temp);
-        DATA.city_weather = r.weather[0].description;
-        DATA.city_weather_icon = r.weather[0].icon;
-        DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('fr-FR', {
-          hour: '2-digit',
-          minute: '2-digit',
-          timeZone: 'Europe/Paris',
-        });
-        DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('fr-FR', {
-          hour: '2-digit',
-          minute: '2-digit',
-          timeZone: 'Europe/Paris',
-        });
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Stockholm',
       });
-  }
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Stockholm',
+      });
+    });
+}
 
 function generateReadMe() {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
@@ -52,12 +45,10 @@ function generateReadMe() {
     fs.writeFileSync('README.md', output);
   });
 }
-generateReadMe();
 
 async function action() {
-    await setWeatherInformation();
-    await generateReadMe();
+  await setWeatherInformation();
+  await generateReadMe();
 }
-  
+
 action();
-  
